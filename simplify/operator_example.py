5d84e8b829be7b6b363c12e538b7ad7f1e7c68ed
darrylweimers <darrylweimers@gmail.com>
2018-08-15 22:17:30 -0400

    variables & operator

---
+++ simplify/operator_example.py
@@ -0,0 +1,122 @@
+
+print("""
+----------------------------------------------
+Operator
+
+    a character which represents an action
+
+""")
+
+
+print("""
+----------------------------------------------
+
+Arithmetic Operators
+
+     Name                   operator
+    _____________________________________
+
+    Addition                    +
+    Subtraction                 -
+    Multiplication              *
+    Division                    /
+    Floor                       //
+    Exponent                    **
+    Modulus                     %
+
+Example:
+""")
+
+
+number_1 = 8
+number_2 = 2
+
+result = number_1 + number_2
+print("Addition: {number1} + {number2} = {result}".format(number1=number_1, number2=number_2, result=result))
+
+result = number_1 - number_2
+print("Subtraction: {number1} - {number2} = {result}".format(number1=number_1, number2=number_2, result=result))
+
+result = number_1 * number_2
+print("Multiplication: {number1} - {number2} = {result}".format(number1=number_1, number2=number_2, result=result))
+
+result = number_1 / number_2
+print("Division: {number1} / {number2} = {result}".format(number1=number_1, number2=number_2, result=result))
+
+result = number_1 // number_2
+print("Division: {number1} // {number2} = {result}".format(number1=number_1, number2=number_2, result=result))
+
+result = number_1 ** number_2
+print("Exponent: {number1} ** {number2} = {result}".format(number1=number_1, number2=number_2, result=result))
+
+result = number_1 % number_2
+print("Modulus: {number1} % {number2} = {result}".format(number1=number_1, number2=number_2, result=result))
+
+
+print("""
+----------------------------------------------
+
+Comparison (Relational) Operators
+
+     Name                   operator
+    _____________________________________
+
+    Addition                    +
+    Subtraction                 -
+    Multiplication              *
+    Division                    /
+    Floor                       //
+    Exponent                    **
+    Modulus                     %
+
+Example:
+""")
+
+
+print("""
+----------------------------------------------
+
+Assignment Operators
+
+Example:
+""")
+
+
+
+print("""
+----------------------------------------------
+
+
+Logical Operators
+
+
+Example:
+""")
+
+
+
+print("""
+----------------------------------------------
+
+Bitwise Operators
+
+
+Example:
+""")
+
+
+print("""
+----------------------------------------------
+
+Membership Operators
+
+Example:
+""")
+
+print("""
+----------------------------------------------
+
+Identity Operators
+
+Example:
+""")
--- simplify/variable_example.py
+++ simplify/variable_example.py
@@ -51,3 +51,64 @@ name, identification = "Peter", 43423789
 print("Name:", name)
 print("Identification:", identification)
 
+
+print("""
+----------------------------------------------
+
+BUILT IN DATA TYPE
+
+    Data stored in memory can be of many types. Python offers the following types:
+
+    Numbers
+        int
+        float
+        complex
+    String
+    List
+    Tuple
+    Dictionary
+
+Example:
+""")
+
+integer_number = 10
+float_number = 5.6
+complex_number = 5j
+string = "5"
+a_list = [1, 2, 3]
+a_tuple = (4, 5, 6)
+a_dict = {"1": "one", "2": "two"}
+
+print("int: value->{integer} type->{type}".format(integer=integer_number, type=type(integer_number)))
+print("float: value->{float_num} type->{type}".format(float_num=float_number, type=type(float_number)))
+print("complex: value->{complex_num} type->{type}".format(complex_num=complex_number, type=type(complex_number)))
+print("string: value->{str} type->{type}".format(str=string, type=type(string)))
+print("list: value->{list} type->{type}".format(list=a_list, type=type(a_list)))
+print("tuple: value->{tuple} type->{type}".format(tuple=a_tuple, type=type(a_tuple)))
+print("dict: value->{dict} type->{type}".format(dict=a_dict, type=type(a_dict)))
+
+
+
+print("""
+----------------------------------------------
+
+DATA TYPE CONVERSION
+
+    changes a variable from one data type to another
+
+Example:
+""")
+integer_number = int(float_number)
+float_number = float(integer_number)
+complex_number = complex(integer_number)
+string = str(integer_number)
+a_list = list(a_tuple)
+a_tuple = tuple(a_list)
+
+
+print("int: value->{integer} type->{type}".format(integer=integer_number, type=type(integer_number)))
+print("float: value->{float_num} type->{type}".format(float_num=float_number, type=type(float_number)))
+print("complex: value->{complex_num} type->{type}".format(complex_num=complex_number, type=type(complex_number)))
+print("string: value->{str} type->{type}".format(str=string, type=type(string)))
+print("list: value->{list} type->{type}".format(list=a_list, type=type(a_list)))
+print("tuple: value->{tuple} type->{type}".format(tuple=a_tuple, type=type(a_tuple)))
